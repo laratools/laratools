5321368a9ec2781618714cad1ce108178bb9c973
Wade Urry <wade@iwader.co.uk>
2018-03-20 07:05:04 +0000

    Add withNotices and withSuccess macros

--- src/Providers/LaratoolsServiceProvider.php
+++ src/Providers/LaratoolsServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace Laratools\Providers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\ViewErrorBag;
 
 class LaratoolsServiceProvider extends ServiceProvider
 {
@@ -15,6 +17,46 @@ class LaratoolsServiceProvider extends ServiceProvider
 
     public function register()
     {
-        //
+        $this->registerRedirectResponseMacros();
+    }
+
+    protected function registerRedirectResponseMacros()
+    {
+        if (! RedirectResponse::hasMacro('withNotices')) {
+            RedirectResponse::macro('withNotices', function ($provider, $key = 'default') {
+                /** @var RedirectResponse $this */
+                $value = $this->parseErrors($provider);
+
+                $notices = $this->session->get('notices', new ViewErrorBag());
+
+                if (! $notices instanceof ViewErrorBag) {
+                    $notices = new ViewErrorBag();
+                }
+
+                $this->session->flash(
+                    'notices', $notices->put($key, $value)
+                );
+
+                return $this;
+            });
+        }
+
+        if (! RedirectResponse::hasMacro('withSuccesses')) {
+            RedirectResponse::macro('withSuccesses', function ($provider, $key = 'default') {
+                $value = $this->parseErrors($provider);
+
+                $successes = $this->session->get('successes', new ViewErrorBag());
+
+                if (! $successes instanceof ViewErrorBag) {
+                    $successes = new ViewErrorBag();
+                }
+
+                $this->session->flash(
+                    'successes', $successes->put($key, $value)
+                );
+
+                return $this;
+            });
+        }
     }
 }
